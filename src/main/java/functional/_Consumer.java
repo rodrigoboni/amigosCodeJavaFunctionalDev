1d4bae1568dd6620f744f65f68ba39cb28e921c1
rodrigoboni <rodrigoboni@gmail.com>
2021-09-03 17:12:46 -0300

    first

---
+++ src/main/java/functional/_Consumer.java
@@ -0,0 +1,33 @@
+package functional;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Zé roela", "123456789");
+
+        // normal method
+        greetCustomer(customer);
+
+        // functional / consumer way
+        greetCustomerConsumer.accept(customer);
+    }
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.name + ", thanks for registering phone " + customer.phone);
+    }
+
+    // Consumer it's a functional interface that consume / accepts one input and has no return
+    // BiConsumer it's the same idea except for it accepts 2 parameters
+    static Consumer<Customer> greetCustomerConsumer = customer -> System.out.println("Hello " + customer.name + ", thanks for registering phone " + customer.phone);
+
+    static class Customer {
+        private final String name;
+        private final String phone;
+
+        public Customer(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+        }
+    }
+}
